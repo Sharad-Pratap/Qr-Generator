22d78feef2acf1649c76f2c3a7581bf22e547fef
Sharad-Pratap <sharadpratap40@gmail.com>
2022-12-10 21:28:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect,useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>{
+  const [Inputvalue,setInputValue] = useState("")
+  const [isActive,setisActive] = useState(false)
+  const [word,setWord] = useState("")
+  const [qrCode, setQrCode] = useState("")
+
+  useEffect(()=>{
+    setQrCode(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${word}`)
+  },[word])
+
+  let handleClick = () =>{
+    if(Inputvalue !== "")
+    {
+      setisActive(true)
+      setWord(Inputvalue)
+    }
+  }
+  return(
+     <div className='container' style={{height:isActive ?  "450px" : ""}}>
+      <h3 className='title'><i class="fa-sharp fa-solid fa-qrcode"></i> Qr Code Generator</h3>
+      <div className='qr-code-box' style={{visibility:isActive ? "visible": ""}}>
+        <div className='qr-code'>
+          <img src={qrCode} alt=''/>
+        </div>
+      </div>
+      <input type='text' className='userInput' placeholder='Enter text or URL' onChange=
+      {(e)=>setInputValue(e.target.value)} style={{top:isActive ? "88px" : ""}}/>
+      <button className='generate-btn' onClick={handleClick}>Generate</button>
+     </div>
+     )
+
 }
-
 export default App;
